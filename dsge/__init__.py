7e23dd6699596e2456cf016b0530deea3fe64c55
apsql <andreapasqualini@outlook.com>
2017-09-19 19:23:53 +0200

    Corrected typo in main docstring

--- dsge/__init__.py
+++ dsge/__init__.py
@@ -1,6 +1,6 @@
 """
 This module has been developed specifically with the application in mind,
-so it is not necessary the best code in the universe for the job.
+so it is not necessarily the best code in the universe for the job.
 It mainly serves as a reference for the exercises I do following the textbook
 'Bayesian Estimation of DSGE Models' by E.P. Herbst and F. Schorfheide.
 Sometimes I draw some inspiration also from https://lectures.quantecon.org/py/.
