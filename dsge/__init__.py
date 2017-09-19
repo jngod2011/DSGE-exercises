4f4e0280d28d8c0ff6deb9171485c8d4371ecfdd
apsql <andreapasqualini@outlook.com>
2017-09-19 18:53:44 +0200

    Added LaTeX documentation/notes and modified DSGE module.

---
+++ dsge/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module has been developed specifically with the application in mind,
+so it is not necessary the best code in the universe for the job.
+It mainly serves as a reference for the exercises I do following the textbook
+'Bayesian Estimation of DSGE Models' by E.P. Herbst and F. Schorfheide.
+Sometimes I draw some inspiration also from https://lectures.quantecon.org/py/.
+"""
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+
